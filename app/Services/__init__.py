81e49d8ff29206ba0c273f100d10750b66dc6293
Gammill32 <Gammill32@gmail.com>
2025-02-17 22:43:18 -0600

    Update __init__.py to correct file name

--- app/Services/__init__.py
+++ app/Services/__init__.py
@@ -1,5 +1,5 @@
 # app/services/__init__.py
-from .game_night_service import (
+from .index_service import (
     get_game_nights,
     get_earliest_game_night,
     get_recent_and_future_game_nights,
