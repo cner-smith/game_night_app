2e383923e1c4844490ed9b4cdd6e3f2dc6e83861
Gammill32 <Gammill32@gmail.com>
2025-02-17 23:03:32 -0600

    Delete app/Services directory

--- app/Services/__init__.py
+++
@@ -1,8 +0,0 @@
-# app/services/__init__.py
-from .index_service import (
-    get_game_nights,
-    get_earliest_game_night,
-    get_recent_and_future_game_nights,
-    get_calendar_data,
-    get_navigation_dates
-)
