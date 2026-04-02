73c0379063c994925e220cf9abd80c5c8dadf9f4
Conner Smith <cner.smith@gmail.com>
2026-04-02 09:49:20 -0500

    fix: tell ruff flask_session is third-party to fix import sort

--- app/extensions.py
+++ app/extensions.py
@@ -3,9 +3,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 migrate = Migrate()
